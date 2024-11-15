0f4118d700513b0fe6b1b7b66d69d97dc62062f2
Ankush-accio <ankushrohilla2304@gmail.com>
2024-11-15 14:43:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let freqObj = {}
+	if(str.length = 0){
+	return null
+	}
+	for (let i = 0; i < str.length; i++) {
+		
+		if(!freqObj[str.charAt(i)])
+		{
+			freqObj[str.charAt(i)] = 1
+		}
+	esle
+	{
+		freqObj[str.charAt(i)] = freqObj[str.charAt(i)] + 1
+	}
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+	for(let item in freqObj){
+		if(freqObj[item] == 1){
+			return item
+		}
+	}
+	return null
+}
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
